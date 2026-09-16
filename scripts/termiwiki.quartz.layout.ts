f0a84f7ef343d19c058e87d4719f531486a386b3
Skippy <philip@mcgaw.eu>
2026-09-16 21:35:57 +0100

    Style TermiPeople images as responsive right-floated portraits

--- scripts/termiwiki.quartz.layout.ts
+++ scripts/termiwiki.quartz.layout.ts
@@ -1,8 +1,42 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import type { QuartzComponent } from "./quartz/components/types"
+
+const TermiWikiHead: QuartzComponent = (props) => {
+  const Head = Component.Head()
+
+  return (
+    <>
+      <Head {...props} />
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+            body[data-slug^="TermiPeople/"] .popover-hint img {
+              float: right;
+              width: 280px;
+              max-width: 40%;
+              height: auto;
+              margin: 0 0 1rem 1.5rem;
+            }
+
+            @media (max-width: 700px) {
+              body[data-slug^="TermiPeople/"] .popover-hint img {
+                float: none;
+                display: block;
+                width: auto;
+                max-width: 100%;
+                margin: 1rem auto;
+              }
+            }
+          `,
+        }}
+      />
+    </>
+  )
+}
 
 export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
+  head: TermiWikiHead,
   header: [],
   afterBody: [],
   footer: Component.Footer(),
